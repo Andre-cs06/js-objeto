d185849e7d1dfdafde9dd3d4950ba12a5ada493f
André Souza <andrecs.06@gmail.com>
2022-10-19 22:41:56 -0300

    new file

---
+++ objetos.js
@@ -0,0 +1,10 @@
+const listaCPFs = ['12345678912', '98765432198', '14785236914']
+
+//const clientes = ['Idade', 'André', 'Idade', 36]
+
+const cliente = {
+    nome:'André',
+    idade: 36,
+    cpf: '12345678912',
+    email: 'andre@email'
+}
---
+++ print-infos.js
@@ -0,0 +1,10 @@
+const cliente = {
+    nome:'André',
+    idade: 36,
+    cpf: '12345678912',
+    email: 'andre@email'
+}
+
+console.log(`Meu nome é ${cliente.nome} e tenho ${cliente.idade}`)
+
+console.log(cliente.cpf.substring(0,3))
